b06cd95f416a7e409b50d1289444956a3a3a6383
Geoffrey Koros <gkoros@mobileaccord.com>
2018-11-28 16:04:40 +0300

    Add hooks to edit the android Manifest xml

--- scripts/android/editManifest.js
+++ scripts/android/editManifest.js
@@ -34,26 +34,5 @@ module.exports = function(context) {
         deferred.reject(new Error("Can't find AndroidManifest.xml"))
     }
 
-    var filepath = filepaths.find(function(filepath) {
-        try {
-            fs.accessSync(filepath, fs.constants.F_OK)
-            return true
-        } catch (err) {
-            return false
-        }
-    })
-	
-	var settingsPath = './platforms/android/settings.gradle';
-
-    if (filepath != null) {
-		srcContents = fs.readFile(settingsPath, 'utf8') + '\ninclude ":libSyncNowDetector"';
-        fs.writeFileSync(settingsPath, srcContents)
-		console.log(srcContents);
-		console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
-        deferred.resolve()
-    } else {
-        deferred.reject(new Error("Can't find settings.gradle.xml"))
-    }
-
     return deferred.promise
 }
--- www/KantarSyncNow.js
+++ www/KantarSyncNow.js
@@ -1,11 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'KantarSyncNow', 'coolMethod', [arg0]);
-};
 exports.invokeStartDetect = function (success, error) {
     exec(success, error, 'KantarSyncNow', 'invokeStartDetect',[]);
-};
-exports.startDetection = function (success, error) {
-    exec(success, error, 'KantarSyncNow', 'startDetection',[]);
 };
